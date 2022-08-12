e24d130032fc7ef699e5233669125db982ce7b50
liaojh <liaojh@augurit.com>
2022-08-12 17:04:07 +0800

    :sparkles:feat: 增加promise的一些静态方法实现；

---
+++ promise/promiseFinally.js
@@ -0,0 +1,23 @@
+// finally 方法不管是调用了 then 还是 catch，最终都会执行到 finally 的 callback
+Promise.prototype.finally = function (callback) {
+  // this 是 Promise.prototype， this.constructor 返回 Promise 构造函数
+  let P = this.constructor; // 让P可以调用resolve()\reject() 等静态方法
+  return this.then( // 注意：P.then将返回undefined
+    (value) => P.resolve(callback()).then(() => value), //同理：this.resolve, this.reject将返回undefined
+    (reason) =>
+      P.resolve(callback()).then(() => {
+        throw reason;
+      })
+  );
+};
+
+// Promise  // 构造函数
+// Promise.prototype.then
+// Promise.prototype.catch
+// Promise.prototype.finally
+ 
+// // 静态方法
+// Promise.resolve
+// Promise.reject
+// Promise.race
+// Promise.all

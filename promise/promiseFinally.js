71777191e9105d847844945b9f542f4b7d8f94da
liaojh <liaojh@augurit.com>
2022-08-18 15:42:55 +0800

    :memo:docs: 修改promise.finally中的注释；

--- promise/promiseFinally.js
+++ promise/promiseFinally.js
@@ -2,7 +2,8 @@
 Promise.prototype.finally = function (callback) {
   // this 是 Promise.prototype， this.constructor 返回 Promise 构造函数
   let P = this.constructor; // 让P可以调用resolve()\reject() 等静态方法
-  return this.then( // 注意：P.then将返回undefined
+  return this.then(
+    // 注意：P.then将返回undefined， （Promise实例具有then方法，也就是说，then方法是定义在原型对象Promise.prototype上的）
     (value) => P.resolve(callback()).then(() => value), //同理：this.resolve, this.reject将返回undefined
     (reason) =>
       P.resolve(callback()).then(() => {
@@ -15,9 +16,11 @@ Promise.prototype.finally = function (callback) {
 // Promise.prototype.then
 // Promise.prototype.catch
 // Promise.prototype.finally
- 
+
 // // 静态方法
-// Promise.resolve
-// Promise.reject
-// Promise.race
-// Promise.all
+// Promise.resolve()
+// Promise.reject()
+// Promise.race()
+// Promise.all()
+// Promise.allSettled() 
+// Promise.any() 
